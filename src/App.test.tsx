64b9255d0e76873e37981a620729f91cd9006e3b
oliver-distributed <tembokoliver3@gmail.com>
2022-10-21 15:10:20 -0400

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Jumbotron from './Jumbotron';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Jumbotron component', () => {
+  render(<Jumbotron />);
+  const jumbotronElement = screen.getByTestId('jumbotron');
+  expect(jumbotronElement).toBeInTheDocument();
+  
 });
---
+++ src/Jumbotron.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Jumbotron = () => {
+  return (
+    <div className="App">
+      <p></p>
+    </div>
+  );
+}
+
+export default Jumbotron;
