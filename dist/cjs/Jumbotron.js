bbdfa419a4443932a8c4e86dd02f24b9ba0d0dab
oliver-distributed <tembokoliver3@gmail.com>
2022-10-31 06:18:01 -0400

    update linters

--- dist/cjs/Jumbotron.js
+++ dist/cjs/Jumbotron.js
@@ -3,9 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Jumbotron = void 0;
 var tslib_1 = require("tslib");
 var React = tslib_1.__importStar(require("react"));
+/**
+ * @component Jumbotron
+ * @description Component Jumbotron.
+ * @example
+ * return (
+ *   <Jumbotron backgroundImage="..." />
+ * )
+ */
 exports.Jumbotron = React.forwardRef(function (props, ref) {
-    var backgroundImage = props.backgroundImage, className = props.className, _a = props.height, height = _a === void 0 ? "100vh" : _a, _b = props.backgroundSize, backgroundSize = _b === void 0 ? "cover" : _b, _c = props.backgroundPosition, backgroundPosition = _c === void 0 ? "center" : _c, _d = props.backgroundRepeat, backgroundRepeat = _d === void 0 ? "no-repeat" : _d, style = props.style, children = props.children;
+    var backgroundImage = props.backgroundImage, className = props.className, _a = props.height, height = _a === void 0 ? '100vh' : _a, _b = props.backgroundSize, backgroundSize = _b === void 0 ? 'cover' : _b, _c = props.backgroundPosition, backgroundPosition = _c === void 0 ? 'center' : _c, _d = props.backgroundRepeat, backgroundRepeat = _d === void 0 ? 'no-repeat' : _d, style = props.style, children = props.children;
     return (React.createElement("div", { style: tslib_1.__assign({ backgroundImage: "url(".concat(backgroundImage, ")"), backgroundSize: backgroundSize, backgroundPosition: backgroundPosition, backgroundRepeat: backgroundRepeat, height: height }, style), className: className, ref: ref }, children));
 });
+exports.Jumbotron.displayName = 'Jumbotron';
 exports.default = exports.Jumbotron;
 //# sourceMappingURL=Jumbotron.js.map
--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,4 +1,2 @@
 export { Jumbotron } from './Jumbotron';
-// export type { JumbotronProps } from './Jumbotron';
-// export { default } from './Jumbotron';
 //# sourceMappingURL=index.js.map
